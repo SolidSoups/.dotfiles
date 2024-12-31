d6368b09321dc027b7132403a035b0de8c27c832
SolidSoups <156454005+SolidSoups@users.noreply.github.com>
2024-12-31 03:21:00 +0100

    Added more config files for specs with long config code

---
+++ nvim/.config/nvim/lua/elias/config/nvim-cmp.lua
@@ -0,0 +1,40 @@
+local cmp = require("cmp")
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body)
+    end,
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-y"] = cmp.mapping.confirm({ select = true }),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+  }),
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "nvim_lsp_signature_help" },
+    { name = "nvim_lua" },
+    { name = "path" },
+    { name = "buffer" },
+  },
+})
+-- Use buffer source ofr '/' and '?' (if you enabled 'native_menu', this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' },
+  },
+})
+
+cmp.event:on("confirm_done", function(args)
+  require("conform").format({ bufnr = args.buf, async = false, timeout_ms = 5000 })
+end)
